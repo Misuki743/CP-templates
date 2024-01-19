2d86a04e5a21c7b3c7de53f4fda9c67a0d3579c7
Misuki743 <goole0goole@gmail.com>
2024-01-20 01:00:17 +0800

    a

---
+++ Point_Set_Range_Composite.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
+#include "tt.cpp"
+#include "MontgomeryModInt.cpp"
+#include "segmentTree.cpp"
+
+using line = array<mint, 2>;
+line unit() { return line{1, 0}; }
+line ope(const line &l, const line &r) {
+  return {l[0] * r[0], l[1] * r[0] + r[1]};
+}
+
+signed main() {
+  ios::sync_with_stdio(false), cin.tie(NULL);
+
+  int n, q; cin >> n >> q;
+  vector<line> a(n);
+  for(auto &[c, d] : a)
+    cin >> c >> d;
+
+  segmentTree<line, unit, ope> st(a);
+  while(q--) {
+    int t, x, y, z; cin >> t >> x >> y >> z;
+    if (t == 0) {
+      st.set(x, line{y, z});
+    } else {
+      line l = st.query(x, y);
+      cout << l[0] * z + l[1] << '\n';
+    }
+
+  }
+
+  return 0;
+}
--- t.cpp
+++ t.cpp
@@ -1,10 +1,63 @@
-#include<bits/stdc++.h>
-#define int ll
+#include <algorithm>
+#include <array>
+#include <bit>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cfenv>
+#include <cfloat>
+#include <chrono>
+#include <cinttypes>
+#include <climits>
+#include <cmath>
+#include <compare>
+#include <complex>
+#include <concepts>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <new>
+#include <numbers>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <ranges>
+#include <set>
+#include <span>
+#include <sstream>
+#include <stack>
+#include <streambuf>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <variant>
+
+//#define int ll
 #define INT128_MAX (__int128)(((unsigned __int128) 1 << ((sizeof(__int128) * __CHAR_BIT__) - 1)) - 1)
 #define INT128_MIN (-INT128_MAX - 1)
 
-//#pragma GCC optimize("O2", "tree-vectorize", "unroll-loops")
-//#pragma GCC target("avx2", "bmi", "bmi2")
+#ifdef DEBUG
+#define dbg(x) cout << (#x) << " = " << x << '\n'
+#else
+#define dbg(x)
+#endif
 
 namespace R = std::ranges;
 namespace V = std::views;
@@ -13,16 +66,9 @@ using namespace std;
 
 using ll = long long;
 using ull = unsigned long long;
-using pii = pair<int, int>;
-using pll = pair<long long, long long>;
-using tiii = tuple<int, int, int>;
 using ldb = long double;
 //#define double ldb
 
-template<class T>
-ostream& operator<<(ostream& os, const pair<T, T> pr) {
-  return os << pr.first << ' ' << pr.second;
-}
 template<class T, size_t N>
 ostream& operator<<(ostream& os, const array<T, N> &arr) {
   for(const T &X : arr)
@@ -35,10 +81,18 @@ ostream& operator<<(ostream& os, const vector<T> &vec) {
     os << X << ' ';
   return os;
 }
+template<class T>
+ostream& operator<<(ostream& os, const set<T> &s) {
+  for(const T &x : s)
+    os << x << ' ';
+  return os;
+}
+
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(NULL);
 
+  
 
   return 0;
 }
