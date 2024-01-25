eef825b96afcf540b682ab99bc927234412b7d43
Misuki743 <goole0goole@gmail.com>
2024-01-26 03:52:48 +0800

    fix mulConvolution

--- poly/mulConvolution.cpp
+++ poly/mulConvolution.cpp
@@ -1,11 +1,46 @@
-//#include "numtheory/primitiveRoot.cpp"
-//#include "numtheory/fastFactorize.cpp"
 //#include "poly/NTTmint.cpp"
 //#include "modint/MontgomeryModInt.cpp"
 
 struct mulConvolution {
   const int P, root;
   vector<int> powR, logR;
+
+  int primitiveRoot(int p) {
+    vector<int> pf;
+    {
+      int tmp = p - 1;
+      for(int i = 2; i * i <= (p - 1); i++) {
+        if (tmp % i != 0) continue;
+        pf.emplace_back(i);
+        while(tmp % i == 0) tmp /= i;
+      }
+      if (tmp != 1)
+        pf.emplace_back(tmp);
+    }
+
+    auto modPow = [p](ll a, int x) -> int {
+      if (x == 0) return 1;
+      if (a == 0) return 0;
+      ll b = 1;
+      while(x) {
+        if (x & 1) b = b * a % p;
+        a = a * a % p, x >>= 1;
+      }
+      return b;
+    };
+
+    for(int r = 1; ; r++) {
+      bool isRoot = true;
+      for(int d : pf) {
+        if (modPow(r, (p - 1) / d) == 1) {
+          isRoot = false;
+          break;
+        }
+      }
+      if (isRoot)
+        return r;
+    }
+  }
 
   mulConvolution(int _P) : P(_P), root(primitiveRoot(_P)), powR(P - 1), logR(P, -1) {
     for(int i = 0, tmp = 1; i < P - 1; i++, tmp = (ll)tmp * root % P)
@@ -32,12 +67,14 @@ struct mulConvolution {
 
   template<class Mint>
   vector<Mint> mulConv(vector<Mint> a, vector<Mint> b, vector<Mint>(*conv)(vector<Mint>, vector<Mint>)) {
+    Mint zero = accumulate(a.begin(), a.end(), mint(0)) * b[0] + accumulate(b.begin() + 1, b.end(), mint(0)) * a[0];
     a = transform(a), b = transform(b);
     a = conv(a, b);
     for(int i = P - 1; i < 2 * P - 3; i++)
       a[i - (P - 1)] += a[i];
     a.resize(P - 1);
     a = invTransform(a);
+    a[0] = zero;
     return a;
   }
 };
--- test/yuki_2613.test.cpp
+++ test/yuki_2613.test.cpp
@@ -3,8 +3,6 @@
 #include "../default/t.cpp";
 #include "../modint/MontgomeryModInt.cpp"
 #include "../poly/NTTmint.cpp"
-#include "../numtheory/fastFactorize.cpp"
-#include "../numtheory/primitiveRoot.cpp"
 #include "../poly/mulConvolution.cpp"
 
 int p;
@@ -24,7 +22,9 @@ signed main() {
   fac[0] = 1;
   for(int i = 1; i < p; i++)
     fac[i] = (ll)fac[i - 1] * i % p;
-  facInv[p - 1] = modpow(fac[p - 1], p - 2, p);
+  facInv[p - 1] = 1;
+  for(int i = 0; i < p - 2; i++)
+    facInv[p - 1] = (ll)facInv[p - 1] * fac[p - 1] % p;
   for(int i = p - 2; i >= 0; i--)
     facInv[i] = (ll)facInv[i + 1] * (i + 1) % p;
 
