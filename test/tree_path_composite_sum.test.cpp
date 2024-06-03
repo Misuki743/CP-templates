2e83c21bd4bbf990153f78a3f88dcfa2e06d9e5b
Misuki743 <goole0goole@gmail.com>
2024-06-04 01:01:58 +0800

    a

--- test/tree_path_composite_sum.test.cpp
+++ test/tree_path_composite_sum.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/tree_path_composite_sum"
 
 #include "../default/t.cpp"
-#include "../modint/MontgomeryModint.cpp"
+#include "../modint/MontgomeryModInt.cpp"
 #include "../misc/rerootingDP.cpp"
 
 using V = array<mint, 2>;
