bfb6d4359a781f2026d5a6fd0f65c7d10a72b57a
Misuki743 <goole0goole@gmail.com>
2024-09-21 11:08:45 +0800

    a

--- ds/slope_trick.cpp
+++ ds/slope_trick.cpp
@@ -2,23 +2,23 @@ template<class T>
 struct slope_trick {
   max_heap<T> L;
   min_heap<T> R;
-  T mn, tag;
+  T mn = T(0), Ltag = T(0), Rtag = T(0);
 
   //(x - a)+
   void add_x_minus_a(T a) {
     if (!L.empty())
-      mn += max((L.top() + tag) - a, T(0));
-    L.push(a - tag);
-    R.push(L.top());
+      mn += max((L.top() + Ltag) - a, T(0));
+    L.push(a - Ltag);
+    R.push(L.top() + Ltag - Rtag);
     L.pop();
   }
 
   //(a - x)+
   void add_a_minus_x(T a) {
     if (!R.empty())
-      mn += max(a - (R.top() + tag), T(0));
-    R.push(a - tag);
-    L.push(R.top());
+      mn += max(a - (R.top() + Rtag), T(0));
+    R.push(a - Rtag);
+    L.push(R.top() + Rtag - Ltag);
     R.pop();
   }
 
@@ -36,21 +36,23 @@ struct slope_trick {
     L.swap(x);
   }
 
-  void shift(T d) { tag += d; }
+  void slide_min(T d) { (d > 0 ? Rtag : Ltag) += d; }
+
+  void shift(T d) { Ltag += d, Rtag += d; }
 
   T eval(T x) {
     T y = mn;
     {
       max_heap<T> tmp = L;
       while(!tmp.empty()) {
-        y += max((tmp.top() + tag) - x, T(0));
+        y += max((tmp.top() + Ltag) - x, T(0));
         tmp.pop();
       }
     }
     {
       min_heap<T> tmp = R;
       while(!tmp.empty()) {
-        y += max(x - (tmp.top() + tag), T(0));
+        y += max(x - (tmp.top() + Rtag), T(0));
         tmp.pop();
       }
     }
@@ -60,7 +62,7 @@ struct slope_trick {
   T query_min() { return mn; }
 
   array<T, 2> argmin() {
-    return {L.empty() ? numeric_limits<T>::min() : L.top() + tag,
-            R.empty() ? numeric_limits<T>::max() : R.top() + tag};
+    return {L.empty() ? numeric_limits<T>::min() : L.top() + Ltag,
+            R.empty() ? numeric_limits<T>::max() : R.top() + Rtag};
   }
 };
