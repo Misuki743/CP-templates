091d370eaadfa57d288b273b75453e6ccfdd95fa
Misuki743 <goole0goole@gmail.com>
2025-02-03 22:00:03 +0800

    a

--- graph/BFS.cpp
+++ graph/BFS.cpp
@@ -1,7 +1,6 @@
-pair<vector<int>, vector<int>> BFS(vector<vector<int>> &g, int s) {
+array<vector<int>, 2> BFS(vector<vector<int>> &g, int s) {
   int n = ssize(g);
   vector<int> dis(n, -1), pre(n, -1);
-  vector<bool> vis(n, false);
   queue<int> q;
 
   dis[s] = 0;
@@ -9,15 +8,12 @@ pair<vector<int>, vector<int>> BFS(vector<vector<int>> &g, int s) {
 
   while(!q.empty()) {
     int v = q.front(); q.pop();
-    if (vis[v]) continue;
-    vis[v] = true;
     for(int x : g[v]) {
-      if (dis[x] == -1) {
-        dis[x] = dis[v] + 1, pre[x] = v;
-        q.push(x);
-      }
+      if (dis[x] != -1) continue;
+      dis[x] = dis[v] + 1, pre[x] = v;
+      q.push(x);
     }
   }
 
-  return make_pair(dis, pre);
+  return {dis, pre};
 }
