88d31580c6181a3e239c23dd35c70ad9b81309dc
Misuki743 <goole0goole@gmail.com>
2025-03-16 20:37:06 +0800

    a

--- geometry/line.cpp
+++ geometry/line.cpp
@@ -1,60 +1,60 @@
 template<class P> bool onSegment(P s, P e, P p) {
-	return p.cross(s, e) == 0 && (s - p).dot(e - p) <= 0;
+  return p.cross(s, e) == 0 && (s - p).dot(e - p) <= 0;
 } //Use segDist(s,e,p)<=epsilon instead when using Point<double>.
 
 template<class P> vector<P> segInter(P a, P b, P c, P d) {
-	auto oa = c.cross(d, a), ob = c.cross(d, b),
-	     oc = a.cross(b, c), od = a.cross(b, d);
-	// Checks if intersection is single non-endpoint point.
-	if (sgn(oa) * sgn(ob) < 0 && sgn(oc) * sgn(od) < 0)
-		return {(a * ob - b * oa) / (ob - oa)};
-	set<P> s;
-	if (onSegment(c, d, a)) s.insert(a);
-	if (onSegment(c, d, b)) s.insert(b);
-	if (onSegment(a, b, c)) s.insert(c);
-	if (onSegment(a, b, d)) s.insert(d);
-	return {all(s)};
+  auto oa = c.cross(d, a), ob = c.cross(d, b),
+       oc = a.cross(b, c), od = a.cross(b, d);
+  // Checks if intersection is single non-endpoint point.
+  if (sgn(oa) * sgn(ob) < 0 && sgn(oc) * sgn(od) < 0)
+    return {(a * ob - b * oa) / (ob - oa)};
+  set<P> s;
+  if (onSegment(c, d, a)) s.insert(a);
+  if (onSegment(c, d, b)) s.insert(b);
+  if (onSegment(a, b, c)) s.insert(c);
+  if (onSegment(a, b, d)) s.insert(d);
+  return {all(s)};
 }
 
 template<class P>
 P lineProj(P a, P b, P p, bool refl=false) {
-	P v = b - a;
-	return p - v.perp()*(1+refl)*v.cross(p-a)/v.dist2();
+  P v = b - a;
+  return p - v.perp()*(1+refl)*v.cross(p-a)/v.dist2();
 }
 
 template<class P> vector<P> segInter(P a, P b, P c, P d) {
-	auto oa = c.cross(d, a), ob = c.cross(d, b),
-	     oc = a.cross(b, c), od = a.cross(b, d);
-	// Checks if intersection is single non-endpoint point.
-	if (sgn(oa) * sgn(ob) < 0 && sgn(oc) * sgn(od) < 0)
-		return {(a * ob - b * oa) / (ob - oa)};
-	set<P> s;
-	if (onSegment(c, d, a)) s.insert(a);
-	if (onSegment(c, d, b)) s.insert(b);
-	if (onSegment(a, b, c)) s.insert(c);
-	if (onSegment(a, b, d)) s.insert(d);
-	return {all(s)};
+  auto oa = c.cross(d, a), ob = c.cross(d, b),
+       oc = a.cross(b, c), od = a.cross(b, d);
+  // Checks if intersection is single non-endpoint point.
+  if (sgn(oa) * sgn(ob) < 0 && sgn(oc) * sgn(od) < 0)
+    return {(a * ob - b * oa) / (ob - oa)};
+  set<P> s;
+  if (onSegment(c, d, a)) s.insert(a);
+  if (onSegment(c, d, b)) s.insert(b);
+  if (onSegment(a, b, c)) s.insert(c);
+  if (onSegment(a, b, d)) s.insert(d);
+  return {all(s)};
 }
 
 typedef Point<double> P;
 double segDist(P& s, P& e, P& p) {
-	if (s==e) return (p-s).dist();
-	auto d = (e-s).dist2(), t = min(d,max(.0,(p-s).dot(e-s)));
-	return ((p-s)*d-(e-s)*t).dist()/d;
+  if (s==e) return (p-s).dist();
+  auto d = (e-s).dist2(), t = min(d,max(.0,(p-s).dot(e-s)));
+  return ((p-s)*d-(e-s)*t).dist()/d;
 }
 
 template<class P>
 double lineDist(const P& a, const P& b, const P& p) {
-	return (double)(b-a).cross(p-a)/(b-a).dist();
+  return (double)(b-a).cross(p-a)/(b-a).dist();
 } //signed distance
 
 //If a unique intersection point of the lines going through s1,e1 and s2,e2 exists \{1, point\} is returned.
 //If no intersection point exists \{0, (0,0)\} is returned and if infinitely many exists \{-1, (0,0)\} is returned.
 template<class P>
 pair<int, P> lineInter(P s1, P e1, P s2, P e2) {
-	auto d = (e1 - s1).cross(e2 - s2);
-	if (d == 0) // if parallel
-		return {-(s1.cross(e1, s2) == 0), P(0, 0)};
-	auto p = s2.cross(e1, e2), q = s2.cross(e2, s1);
-	return {1, (s1 * p + e1 * q) / d};
+  auto d = (e1 - s1).cross(e2 - s2);
+  if (d == 0) // if parallel
+    return {-(s1.cross(e1, s2) == 0), P(0, 0)};
+  auto p = s2.cross(e1, e2), q = s2.cross(e2, s1);
+  return {1, (s1 * p + e1 * q) / d};
 }
