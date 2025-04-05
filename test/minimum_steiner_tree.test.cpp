aebb0c665970069e4ecd716450450dbca791eab6
Misuki743 <goole0goole@gmail.com>
2025-04-05 23:26:39 +0800

    a

---
+++ test/minimum_steiner_tree.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/minimum_steiner_tree"
+
+#include "../default/t.cpp"
+#include "../graph/steinerTree.cpp"
+
+signed main() {
+  ios::sync_with_stdio(false), cin.tie(NULL);
+
+  int n, m; cin >> n >> m;
+  vector<tuple<int, int, ll>> e(m);
+  for(auto &[u, v, w] : e) cin >> u >> v >> w;
+  int k; cin >> k;
+  vector<int> s(k);
+  for(int &x : s) cin >> x;
+
+  auto [cost, t] = steiner_tree(n, s, e);
+  cout << cost << ' ' << ssize(t) << '\n';
+  cout << t << '\n';
+
+  return 0;
+}
+
