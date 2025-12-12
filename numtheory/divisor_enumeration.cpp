5e6ddf462f460b0316b8f7c08ce65c797bd925cd
Misuki743 <goole0goole@gmail.com>
2025-12-12 15:07:14 +0800

    a

---
+++ numtheory/divisor_enumeration.cpp
@@ -0,0 +1,20 @@
+template<int32_t C>
+struct divisor_enumeration {
+  using i32 = int32_t;
+  array<i32, C + 1> s;
+  vector<i32> d;
+  divisor_enumeration() {
+    for(int i = 1; i < C; i++)
+      for(int j = i; j < C; j += i)
+        s[j]++;
+    for(int i = 1; i <= C; i++)
+      s[i] += s[i - 1];
+    d.resize(s[C]);
+    for(int i = C - 1; i >= 1; i--)
+      for(int j = i; j < C; j += i)
+        d[--s[j]] = i;
+  }
+  vector<i32> divisor(i32 x) {
+    return vector(d.begin() + s[x], d.begin() + s[x + 1]);
+  }
+};
