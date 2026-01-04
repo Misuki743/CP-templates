f64e28116cb2acb9a3fdef83ebbe68db6c58ebd0
Misuki743 <goole0goole@gmail.com>
2026-01-05 01:47:42 +0800

    a

--- ds/fast_set.cpp
+++ ds/fast_set.cpp
@@ -1,7 +1,8 @@
 struct fast_set {
   using u64 = uint64_t;
+  using i64 = int64_t;
   static constexpr u64 B = 64;
-  int sz;
+  i64 sz;
   vector<vector<u64>> data;
 
   fast_set(u64 _sz) : sz(_sz) {
@@ -14,7 +15,7 @@ struct fast_set {
   fast_set(string s) : sz(ssize(s)) {
     u64 _sz = sz;
     data.pb(vector<u64>(ceilDiv(_sz, B)));
-    for(int i = 0; i < ssize(s); i++)
+    for(i64 i = 0; i < ssize(s); i++)
       data[0][i / B] |= u64(s[i] - '0') << (i % B);
     _sz /= B;
     while(_sz > 1) {
@@ -25,31 +26,31 @@ struct fast_set {
     }
   }
 
-  bool contains(int x) {
+  bool contains(i64 x) {
     return x < sz and (data[0][x / B] >> (x % B) & 1);
   }
 
-  void insert(int x) {
+  void insert(i64 x) {
     if (contains(x)) return;
     assert(x < sz);
     for(auto &v : data)
       v[x / B] |= u64(1) << (x % B), x /= B;
   }
 
-  void erase(int x) {
+  void erase(i64 x) {
     if (x >= sz or !contains(x)) return;
     u64 pre = !(data[0][x / B] &= (-u64(1)) ^ (u64(1) << (x % B)));
     for(auto &v : data | views::drop(1))
       x /= B, pre = !(v[x / B] ^= u64(pre) << (x % B));
   }
 
-  int geq(int x) {
+  i64 geq(i64 x) {
     if (x >= sz) return sz;
-    chmax(x, int(0));
-    for(int i = 0; i < ssize(data) and x / (int)B < ssize(data[i]); i++, x = x / (int)B + 1) {
+    chmax(x, i64(0));
+    for(i64 i = 0; i < ssize(data) and x / (i64)B < ssize(data[i]); i++, x = x / (i64)B + 1) {
       if (data[i][x / B] >> (x % B)) {
-        int y = x;
-        for(int j = i; j >= 0; j--)
+        i64 y = x;
+        for(i64 j = i; j >= 0; j--)
           y = (y + countr_zero(data[j][y / B] >> (y % B))) * B;
         return y / B;
       }
@@ -57,13 +58,13 @@ struct fast_set {
     return sz;
   }
 
-  int leq(int x) {
+  i64 leq(i64 x) {
     if (x < 0) return -1;
     chmin(x, sz - 1);
-    for(int i = 0; i < ssize(data) and x >= 0; i++, x = x / (int)B - 1) {
+    for(i64 i = 0; i < ssize(data) and x >= 0; i++, x = x / (i64)B - 1) {
       if (data[i][x / B] << (B - 1 - x % B)) {
-        int y = x;
-        for(int j = i; j >= 0; j--)
+        i64 y = x;
+        for(i64 j = i; j >= 0; j--)
           y = (y - countl_zero(data[j][y / B] << (B - 1 - y % B))) * B + (B - 1);
         return y / B;
       }
