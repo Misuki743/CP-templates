d2da27fb4a2f8f45d5b796f09c383ceceef03b41
Misuki743 <goole0goole@gmail.com>
2026-01-29 06:13:58 +0800

    a

---
+++ enumerate/enumerate_twelvefold.cpp
@@ -0,0 +1,97 @@
+template<typename F>
+requires invocable<F, vector<int>>
+void enumerate_cartesian_power(int n, int k, F f) {
+  assert(min(n, k) >= 0);
+  vector<int> p(k);
+  auto dfs = [&](int i, auto &self) -> void {
+    if (i == k) {
+      f(p);
+    } else {
+      for(int x = 0; x < n; x++) {
+        p[i] = x;
+        self(i + 1, self);
+      }
+    }
+  };
+  dfs(0, dfs);
+}
+
+template<typename F>
+requires invocable<F, vector<int>>
+void enumerate_permutation(int n, F f) {
+  assert(n >= 0);
+  vector<int> p(n);
+  iota(p.begin(), p.end(), 0);
+  do { f(p); } while(next_permutation(p.begin(), p.end()));
+}
+
+template<typename F>
+requires invocable<F, vector<int>>
+void enumerate_combination(int n, int k, F f) {
+  assert(min(n, k) >= 0);
+  vector<int> p;
+  auto dfs = [&](auto &self) -> void {
+    if (ssize(p) == k) {
+      f(p);
+    } else {
+      for(int x = (p.empty() ? 0 : p.back() + 1); x + k - ssize(p) <= n; x++) {
+        p.emplace_back(x);
+        self(self);
+        p.pop_back();
+      }
+    }
+  };
+  dfs(dfs);
+}
+
+template<typename F, typename INT>
+requires invocable<F, INT>
+void enumerate_subset(INT msk, F f) {
+  for(INT x = msk; x > 0; x = (x - 1) & msk)
+    f(x);
+  f(0);
+}
+
+template<typename F>
+requires invocable<F, vector<int>>
+void enumerate_set_partition(int n, F f) {
+  assert(n >= 0);
+  vector<int> p;
+  int msk = (1 << n) - 1;
+  auto dfs = [&](auto &self) -> void {
+    if (msk == 0) {
+      f(p);
+    } else {
+      int x = msk & (-msk);
+      msk ^= x;
+      enumerate_subset(msk, [&](int sub) {
+        p.emplace_back(sub | x);
+        msk ^= sub;
+        self(self);
+        msk ^= sub;
+        p.pop_back();
+      });
+      msk ^= x;
+    }
+  };
+  dfs(dfs);
+}
+
+template<typename F>
+requires invocable<F, vector<int>>
+void enumerate_multisubset(int n, int sum, F f) {
+  assert(min(n, sum) >= 0);
+  vector<int> p(n);
+  auto dfs = [&](int i, auto &self) -> void {
+    if (i == n) {
+      if (sum == 0) f(p);
+    } else {
+      for(int x = sum; x >= 0; x--) {
+        p[i] = x, sum -= x;
+        self(i + 1, self);
+        sum += x;
+      }
+    }
+  };
+  dfs(0, dfs);
+}
