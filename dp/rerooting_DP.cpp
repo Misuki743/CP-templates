01950df3ddc6236067aee195b673d2a9a242aa35
Misuki743 <goole0goole@gmail.com>
2026-01-29 21:59:03 +0800

    a

--- dp/rerooting_DP.cpp
+++ dp/rerooting_DP.cpp
@@ -1,6 +1,15 @@
-template<class V, V(*base)(int), class E, E(*addEdge)(const V&, int eid),
-E(*op)(const E&, const E&), V(*addVertex)(const E&, int vid)>
-vector<V> rerootingDP(vector<array<int, 2>> e) {
+template<typename R, typename F, typename... Args>
+concept R_invocable = requires(F&& f, Args&&... args) {
+  { std::invoke(std::forward<F>(f), std::forward<Args>(args)...) } -> std::same_as<R>;
+};
+
+template<class V, class E, typename BASE, typename ADD_EDGE, typename OP, typename ADD_VERTEX>
+requires
+  R_invocable<V, BASE, int> &&
+  R_invocable<E, ADD_EDGE, const V, int> &&
+  R_invocable<E, OP, const E, const E> &&
+  R_invocable<V, ADD_VERTEX, const E, int>
+vector<V> rerooting_DP(vector<array<int, 2>> e, BASE base, ADD_EDGE add_edge, OP op, ADD_VERTEX add_vertex) {
   int n = ssize(e) + 1;
   vector<vector<int>> g(n);
   for(int i = 0; auto [u, v] : e)
@@ -16,11 +25,11 @@ vector<V> rerootingDP(vector<array<int, 2>> e) {
       if (x == p) continue;
       self(x, v, self);
       if (leaf)
-        prod = addEdge(data[x], eid), leaf = false;
+        prod = add_edge(data[x], eid), leaf = false;
       else
-        prod = op(prod, addEdge(data[x], eid));
+        prod = op(prod, add_edge(data[x], eid));
     }
-    if (!leaf) data[v] = addVertex(prod, v);
+    if (!leaf) data[v] = add_vertex(prod, v);
   };
 
   precalc(0, -1, precalc);
@@ -31,12 +40,12 @@ vector<V> rerootingDP(vector<array<int, 2>> e) {
     vector<E> pre(deg), suf(deg);
     for(int i = 0; int eid : g[v]) {
       int x = e[eid][0] ^ e[eid][1] ^ v;
-      pre[i] = suf[i] = addEdge(data[x], eid), i++;
+      pre[i] = suf[i] = add_edge(data[x], eid), i++;
     }
     for(int i = 1; i < deg; i++) pre[i] = op(pre[i - 1], pre[i]);
     for(int i = deg - 2; i >= 0; i--) suf[i] = op(suf[i], suf[i + 1]);
     V tmp = data[v];
-    ret[v] = data[v] = (deg ? addVertex(suf[0], v) : base(v));
+    ret[v] = data[v] = (deg ? add_vertex(suf[0], v) : base(v));
     for(int i = 0; int eid : g[v]) {
       int x = e[eid][0] ^ e[eid][1] ^ v;
       if (x != p) {
@@ -45,7 +54,7 @@ vector<V> rerootingDP(vector<array<int, 2>> e) {
         if (i > 0) prod = pre[i - 1], leaf = false;
         if (i + 1 < deg) prod = (leaf ? suf[i + 1] : op(prod, suf[i + 1])), leaf = false;
         V tmp2 = data[v];
-        data[v] = (leaf ? base(v) : addVertex(prod, v));
+        data[v] = (leaf ? base(v) : add_vertex(prod, v));
         self(x, v, self);
         data[v] = tmp2;
       }
--- test/tree_path_composite_sum.test.cpp
+++ test/tree_path_composite_sum.test.cpp
@@ -2,30 +2,26 @@
 
 #include "../default/t.cpp"
 #include "../modint/MontgomeryModInt.cpp"
-#include "../dp/rerootingDP.cpp"
-
-using V = array<mint, 2>;
-using E = array<mint, 2>;
-
-vector<mint> a, b, c;
-
-V base(int i) { return {a[i], 1}; }
-E addEdge(const V &v, int i) { return {b[i] * v[0] + c[i] * v[1], v[1]}; }
-E op(const E &l, const E &r) { return {l[0] + r[0], l[1] + r[1]}; }
-V addVertex(const E &e, int i) { return {e[0] + a[i], e[1] + 1}; }
+#include "../dp/rerooting_DP.cpp"
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(NULL);
 
   int n; cin >> n;
-  a.resize(n);
+  vector<mint> a(n), b(n - 1), c(n - 1);
   for(mint &x : a) cin >> x;
   vector<array<int, 2>> e(n - 1);
-  b.resize(n - 1), c.resize(n - 1);
   for(int i = 0; i < n - 1; i++)
     cin >> e[i][0] >> e[i][1] >> b[i] >> c[i];
 
-  auto ans = rerootingDP<V, base, E, addEdge, op, addVertex>(e);
+  using V = array<mint, 2>;
+  using E = array<mint, 2>;
+  auto base = [&](int i) { return V{a[i], 1}; };
+  auto add_edge = [&](const V &v, int i) { return E{b[i] * v[0] + c[i] * v[1], v[1]}; };
+  auto op = [&](const E &l, const E &r) { return E{l[0] + r[0], l[1] + r[1]}; };
+  auto add_vertex = [&](const E &e, int i) { return V{e[0] + a[i], e[1] + 1}; };
+
+  auto ans = rerooting_DP<V, E>(e, base, add_edge, op, add_vertex);
   for(int i = 0; i < n; i++)
     cout << ans[i][0] << " \n"[i + 1 == n];
 
